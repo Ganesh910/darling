597acf0f90dbdc8c242fbaa7ff777bdbc0fa717d
pandeyganesh <pandeyganesha@gmail.com>
2023-09-13 16:53:55 +0530

    removed the extra commented content and formatted the file

--- Darling/server.py
+++ Darling/server.py
@@ -6,13 +6,15 @@ app = Flask("Darling")
 
 convo = []
 
+
 @app.route("/verify", methods=['POST'])
 def verify():
     ...
 
+
 @app.route("/", methods=['GET', 'POST'])
 def chat():
-    if request.method=='POST':
+    if request.method == 'POST':
 
         # Time of messaged sent by client
         current_time = time.strftime("%H:%M:%S")
@@ -23,38 +25,14 @@ def chat():
 
         # append the json to the keep the history
         convo.append(json_msg)
-        
+
         print(f"{json_msg['name']} says : {json_msg['msg']}")
 
         return "sent"
-    
-    elif request.method=='GET':
+
+    elif request.method == 'GET':
         ...
 
-# # This route is used to send messages by the client
-# @app.route("/", methods=['GET', 'POST'])
-# def chat():
-#     if request.method == 'POST':
-#         json_msg = request.get_json()
-#         print(f"{json_msg['name']} : {json_msg['msg']}")
-#         return "Sent"
 
-#     else:
-#         return "Not recieved"
-
-# # This route is used when someone wants to connect to you
-# @app.route("/connect", methods=['GET', 'POST'])
-# def connect():
-#     if request.method == 'POST':
-#         json_msg = request.get_json()
-#         print(f"You can now recieve messages from {json_msg['name']}")
-#         return "I'm listening Darling"
-
-#     else :
-#         return "No Darling, We can't talk!"
-    
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
-
-    # localhost:4040/status
-    # http://localhost:4040/api/tunnels
