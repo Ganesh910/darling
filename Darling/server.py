7d2af34e99c22cc7ad3eb83c3dbcab0b7f9a245f
pandeyganesh <pandeyganesha@gmail.com>
2023-09-13 21:26:24 +0530

    appending new_msg and sending them to other users.

--- Darling/server.py
+++ Darling/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import time
 from collections import deque
 
@@ -14,7 +14,8 @@ users = {}
 @app.route("/verify/", methods=['POST'])
 def verify():
     if request.method == 'POST':
-        user_id = 'user' + str(user_id_number)
+        user_id = 'user' + str(time.time())
+        # user_id_number+=1
         q = deque()
         users[user_id] = q
         return user_id
@@ -26,25 +27,46 @@ def verify():
 
 @app.route("/", methods=['GET', 'POST'])
 def chat():
+    print("HERE IS USERS:", users)
     if request.method == 'POST':
 
         # Time of messaged sent by client
         current_time = time.strftime("%H:%M:%S")
         json_msg = request.get_json()
 
-        # add time to the json data
-        json_msg['time'] = current_time
+        if json_msg['purpose']=='post':
 
-        # append the json to the keep the history
-        convo.append(json_msg)
-        users[json_msg['user_id']].append(json_msg)
+            # add time to the json data
+            json_msg['time'] = current_time
 
-        print(f"{json_msg['name']} says : {json_msg['msg']}")
+            # append the json to the keep the history
+            convo.append(json_msg)
 
-        return "sent"
+            # append the message in every inbox
+            for user in users:
+                if user==json_msg['user_id']:
+                    continue
 
-    elif request.method == 'GET':
-        ...
+                users[user].append(json_msg)
+
+
+            print(f"{json_msg['name']} says : {json_msg['msg']}")
+
+            return "sent"
+
+        if json_msg['purpose']=='get':
+            new_msgs = []
+            q = users[json_msg['user_id']]
+            while q:
+                new_msgs.append(q.popleft())
+            return jsonify(new_msgs)
+
+
+        
+        else:
+            print("Purpose Not Defined! ")
+
+    return "Method Not supportes Darling :("
 
 
 if __name__ == "__main__":
