996801f6ed3331f4387ad04307c886db11b6298c
pandeyganesh <pandeyganesha@gmail.com>
2023-09-13 16:54:51 +0530

    Added prompt, send function and partial verify function

--- Darling/client.py
+++ Darling/client.py
@@ -1,28 +1,79 @@
 import requests
 import validators
+import threading
 
-# Establish connection with the server
-def connect(name, url):
+# Prompt for the mode of chatting
 
-    if not validators.url(url):
-        return False
 
-    x = requests.post(url+'/connect', json={'name':name, 'msg':"I want to connect Darling"})
-    if x.status_code == 200:
-        print(f"Connection Established with {name} succesfully")
-        return True
+def prompt():
+    name = input("Enter you name Darling :\n")
+    url = ""
+
+    role = input(
+        "Mode:\n\tType 'server' to create your own server\n\tType 'url' to enter the url\n\tType 'exit' to exit the terminal\n")
+    if role == 'server':
+        url = 'http://127.0.0.1:5000/'
+        print("Server Successfully created!")
+
+    elif role == 'url':
+
+        # ask for the url until it's valid
+        while True:
+            url = input("Enter the Url Darlo!")
+
+            if validators.url(url):
+                break
+            else:
+                print("Darling! It's not valid url. Please Try Again.")
+
+    elif role == 'exit':
+        print("Bye Darling! See you soon!")
+        exit()
+
     else:
-        return False
+        print("Please enter one the command only. (case sensitive)")
 
-# Is used to send messages to server
-def chat(name, url):
+    return (name, url)
+
+
+# Verifies the users. Authentication
+def verify(name, url):
+
+    # Create profile
+    myobj = {'name': name}
+    response = requests.post(url+'/verify/', json=myobj)
+
+    if response.status_code == 200:
+        print("Darling, Your profile is ready")
+    else:
+        print("Sorry Darling! Unable to create your Profile")
+        exit()
+
+# Send messages
+
+
+def send(name, url):
     while True:
         msg = input("=> Darling, ")
-        myobj = {'name':name, 'msg':msg}
+        myobj = {'name': name, 'msg': msg}
 
-        x = requests.post(url+'/', json=myobj)
-        if x.status_code==200:
+        response = requests.post(url+'/', json=myobj)
+        if response.status_code == 200:
             print("Sent")
 
-if __name__=="__main__":
-    chat()
+
+# Receive Messages
+def get():
+    ...
+
+
+data = prompt()
+# verify(*data)
+getThread = threading.Thread(target=get)
+sendThread = threading.Thread(target=send, args=[*data])
+
+getThread.start()
+sendThread.start()
+
+getThread.join()
+sendThread.join()
